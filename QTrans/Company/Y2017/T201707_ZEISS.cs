f2ee58f2497aa66dae120403ae208d8edc293445
hwaust <hwaust@hotmail.com>
2017-12-10 21:40:27 +0800

    Add transducer 201707

---
+++ QTrans/Company/Y2017/T201707_ZEISS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QTrans.Classes
+{
+    class T201707_ZEISS: TransferBase
+    {
+        public override void SetConfig(ParamaterData pd)
+        {
+            base.SetConfig(pd);
+        }
+    }
+}
