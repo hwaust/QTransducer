75895eab3d0a58179b849d863b0c1fd07a9388ac
hwaust <hwaust@hotmail.com>
2017-12-10 22:53:58 +0800

    Update ExcelReader
    @ -1,4 +1,20 @@
    ﻿
    ﻿/***** How to use
     *
     * 更新日期： 2017-12-10
     * 首发日期： 2017-09-01
     * 用法：
     * - Initialize
     * ExcelReader er = new ExcelReader(exlfile);
     *                // er = new ExcelReader(exlfile, MSOfficeVersion.Office2003);
     * - Read data
     * er.getData("A1");      // get cell A1 from the 0-th table.
     * er.getData("C2", 4);  // get cell A1 from the 4-th table.
     *
     * - Show Table
     * er.showTable(0);     // show the 0-th table.
     *
      */

--- QTrans/Classes/ExcelReader.cs
+++ QTrans/Classes/ExcelReader.cs
@@ -1,4 +1,20 @@
-﻿ 
+﻿/***** How to use 
+ * 
+ * 更新日期： 2017-12-10
+ * 首发日期： 2017-09-01
+ * 用法：
+ * - Initialize
+ * ExcelReader er = new ExcelReader(exlfile); 
+ *                // er = new ExcelReader(exlfile, MSOfficeVersion.Office2003);  
+ * - Read data
+ * er.getData("A1");      // get cell A1 from the 0-th table.
+ * er.getData("C2", 4);  // get cell A1 from the 4-th table.
+ * 
+ * - Show Table
+ * er.showTable(0);     // show the 0-th table.
+ * 
+  */
+
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -15,6 +31,44 @@ namespace QDasTransfer.Classes
     {
         public string ErrorMessage { get; internal set; }
 
+        public DataTable[] Tables;
+
+
+        public ExcelReader()
+        {
+
+        }
+
+        public ExcelReader(string excelfile, MSOfficeVersion version = MSOfficeVersion.Office2003)
+        {
+            Tables = LoadSheetsFromExcel(excelfile, version);
+        }
+
+        public void showTable(int v)
+        {
+            DataTable dt = Tables[v];
+
+            StringBuilder sb = new StringBuilder();
+
+            // first line.
+            for (int col = 0; col < dt.Columns.Count; col++)
+            {
+                sb.Append("\t" + (char)(col + 65));
+            }
+            sb.AppendLine();
+
+            for (int row = 0; row < dt.Rows.Count; row++)
+            {
+                sb.Append((row + 1).ToString() + "\t");
+                for (int col = 0; col < dt.Columns.Count ; col++)
+                {
+                    sb.Append(dt.Rows[row][col] + "\t");
+                }
+                sb.AppendLine();
+            }
+
+            Console.WriteLine(sb);
+        }
 
         /// <summary>
         /// 从Excel加载数据表，数据以DataSet集合的形式返回。
@@ -22,7 +76,7 @@ namespace QDasTransfer.Classes
         /// <param name="path"></param>
         /// <param name="version"></param>
         /// <returns></returns>
-        public DataTable[] LoadSheetsFromExcel(string path, MSOfficeVersion version)
+        public DataTable[] LoadSheetsFromExcel(string path, MSOfficeVersion version = MSOfficeVersion.Office2003)
         {
             if (!File.Exists(path))
                 return new DataTable[0];
@@ -60,9 +114,23 @@ namespace QDasTransfer.Classes
                 ErrorMessage = e1.Message;
             }
 
-            return new DataTable[0];
+            return null;
         }
 
 
+        public string getData(string s, int tableindex = 0)
+        {
+            try
+            {
+                string str = s.ToUpper();
+                int row = str[1] - 49;
+                int col = str[0] - 65;
+
+                return Tables[tableindex].Rows[row][col].ToString();
+            }
+            catch { }
+
+            return "";
+        }
     }
 }
