c97289b8f630d6d11f2ea1948f83b0cd892c8e99
hwaust_github <hwaust@hotmail.com>
2017-12-17 18:57:24 +0800

    First complete version of T201707

--- QTrans/Company/Y2017/T201707_ZEISS.cs
+++ QTrans/Company/Y2017/T201707_ZEISS.cs
@@ -15,7 +15,11 @@ namespace QTrans.Company.Y2017
     public class T201707_ZEISS : TransferBase
     {
         string config = ".\\userconfig.ini";
-       
+        int K4092 = -1;
+        int K4062 = -1;
+        int K4072 = -1;
+        int K4272 = -1;
+        QCatalog clog = null;
 
         public override void SetConfig(ParamaterData pd)
         {
@@ -23,18 +27,31 @@ namespace QTrans.Company.Y2017
             CompanyName = "ZEISS";
             VertionInfo = "1.0 alpha";
             pd.SupportAutoTransducer = true;
-            pd.AddExt(".xls"); 
+            pd.AddExt(".xls");
 
             IniAccess ia = new IniAccess(config);
             string catlog = ia.ReadValue("catalog");
 
             if (File.Exists(catlog))
             {
+                clog = QCatalog.load(catlog);
+                if (clog != null)
+                {
+                    int s1 = clog.getCatalogPID("K4272", "001");
+                    int s2 = clog.getCatalogPID("K4092", "P001");
+                    int s3 = clog.getCatalogPID("K4062", "M001");
+                    int s4 = clog.getCatalogPID("K4072", "160");
+                    Console.WriteLine("{0}, {1}, {2}, {3}", s1, s2, s3, s4);
+                }
+                else
+                {
+                    Console.WriteLine("Fialed to read catlog: " + catlog);
+                }
 
             }
             else
             {
-                Console.WriteLine("catlog doex not exist.");
+                Console.WriteLine("catlog '" + catlog + "' does not exist.");
             }
 
 
@@ -70,13 +87,32 @@ namespace QTrans.Company.Y2017
             string K0014 = allinfo.Split('_')[0];
             string K0061 = allinfo.Split('_')[2];
 
+
+            if (clog != null)
+            {
+                K4092 = clog.getCatalogPID("K4092", K0008);
+                K4062 = clog.getCatalogPID("K4062", K0010);
+                K4072 = clog.getCatalogPID("K4072", K0012);
+                K4272 = clog.getCatalogPID("K4272", K0061);
+            }
+
+
             QDataItem qdi = new QDataItem();
             qdi[0006] = K0006;
-            qdi[0008] = K0008;
-            qdi[0010] = K0010;
-            qdi[0012] = K0012;
+
+            if(K4092 >= 0)
+            qdi[0008] = K4092;
+
+            if(K4062 >= 0)
+            qdi[0010] = K4062;
+
+            if(K4072 >= 0)
+                qdi[0012] = K4072;
+
             qdi[0014] = K0014;
-            qdi[0061] = K0061;
+
+            if (K4272 >= 0)
+                qdi[0061] = K4272;
 
             // The first row of data. If it is not fixed, modify this variable.
             int startrow = 13;
@@ -119,7 +155,7 @@ namespace QTrans.Company.Y2017
             string folder = GetOutFolder(path, pd.OutputFolder);
             string timetick = DateTimeHelper.ToYYYYMMDDhhmmssString(DateTime.Now);
             string filename = string.Format("{0}\\{1}_{2}.dfq", folder, allinfo, timetick);
-            
+
             return SaveDfq(qf, filename);
         }
 
