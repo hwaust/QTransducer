7a3887ccf985fce3d69022bdf7db4af23036e658
hao <hwaust@126.com>
2018-10-07 09:26:58 +0800

    Compilable version

--- QTrans/Classes/ExcelReader.cs
+++ QTrans/Classes/ExcelReader.cs
@@ -21,9 +21,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace QDasTransfer.Classes
 {
